ef719d83e1b99049966b4e87d2e39f885e58114c
msilano105 <msilano105@gmail.com>
2016-03-26 12:00:52 -0400

    nothing really

--- Wind Speeds X and Y.R
+++ Wind Speeds X and Y.R
@@ -5,7 +5,6 @@ windspeed = as.numeric(as.character(Weather.Data$Wind.SpeedMPH))
 windspeed[3448] = 0
 windspeed
 theta = (Weather.Data$WindDirDegrees)
-
 Windspeed.X = (windspeed*cos(theta))
 Windspeed.Y = (windspeed*sin(theta))
 Windspeed.X
